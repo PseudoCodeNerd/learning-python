97c2c57a2280fe08c5abd36e07a38e5de13b979f
MaddyDev03 <shekharmadhav03@gmail.com>
2019-06-01 23:10:55 +0530

    Learnt Modules and Packages

--- 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,5 +1,5 @@
-from MyMainPackage.some_main_script import report_main
-from MyMainPackage.SubPackage import mysubscript
+from MyMainPackage.some_main_script import report_main #some_main_script is module name and report_main is function name
+from MyMainPackage.SubPackage import mysubscript#SubPackage is module name and mysubscript is function name
 
 report_main()
 
